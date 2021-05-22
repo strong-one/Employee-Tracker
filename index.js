97b1ea24dd09a99e456b4b0af2e763edfe08b051
Nick Strong <nstrong92@yahoo.com>
2021-05-22 16:17:52 -0600

    building out function frames

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const inquirer = require("inquirer");
+const Choices = require("inquirer/lib/objects/choices");
 
 const mysql = require("mysql");
+
+// const cTable = require("console.table");
 
 //object, method
 const connection = mysql.createConnection({
@@ -17,26 +20,115 @@ const connection = mysql.createConnection({
   database: "employees_db",
 });
 
-connection.connect((err) => {
-  if (err) throw err;
-  console.log(`connected as id ${connection.threadId}`);
-  connection.end();
-});
+// connection.connect((err) => {
+//   if (err) throw err;
+//   console.log(`connected as id ${connection.threadId}`);
+//   start();
+//   //afterConnection();
+//   // ENDS SQL SERVER CONNECTION
+//   //   connection.end();
+// });
 
-const afterConnection = () => {
-  // aysync operation, callback functions, promises, aysync/await
-  // data is what isi returned from the query from database
+// questions for inquirer
+const menuQuestions = [
+  "View all employees",
+  "View all employees by department",
+  "View all employees by manager",
+  "Add employee",
+  "Update employee role",
+  "Update employee manager",
+];
+
+const addEmpQuestions = [
+  "What is the employee's first name",
+  "What is the employee's last name",
+  "What is the employee's role?",
+  "Who is the employees manager?",
+];
+
+// function to show options menu
+const start = () => {
+  inquirer
+    .prompt({
+      name: "mainChoice",
+      type: "list",
+      message: "What would you like to do?",
+      choice: menuQuestions,
+    })
+    .then((answer) => {
+      switch (answer.mainChoice) {
+        case "View all employees":
+          showAllEmps();
+          break;
+        case "View all employees by department":
+          empByDep();
+          break;
+        case "View all employees by manager":
+          empByManager();
+          break;
+        case "Add employee":
+          addEmps();
+          break;
+        case "Update employee role":
+          updateEmp();
+          break;
+        case "Update employee manager":
+          updateManager();
+
+        default:
+          break;
+      }
+    });
+};
+// function to show all employees
+const showAllEmps = () => {
   connection.query("SELECT * FROM employees", (err, res) => {
     if (err) throw err;
-    console.log(res);
-    connection.end();
+    console.table("All employees", res);
   });
+};
+
+const empByDep = () => {
+  connection.query("SELECT * FROM department", (err, res) => {
+    if (err) throw err;
+  });
+};
+
+const empByManager = () => {
+  connection.query((err, res) => {});
+};
+
+const addEmps = () => {
+  connection.query((err, res) => {
+    if (err) throw err;
+    inquirer
+      .prompt({
+        name: "addEmployee",
+        type: "list",
+        message: "Add employee",
+        choice: addEmpQuestions,
+      })
+      .then((answer) => {
+        connection.query("UPDATE employees SET ? WHERE ?", [{}]);
+      });
+  });
+};
+
+const updateEmp = () => {
+  connection.query((err, res) => {});
+};
+
+const updateManager = () => {
+  connection.query((err, res) => {});
 };
 
 connection.connect((err) => {
   if (err) throw err;
   console.log(`connected as id ${connection.threadId}`);
-  afterConnection();
+  start();
+  //afterConnection();
+  // ENDS SQL SERVER CONNECTION
+  //   connection.end();
 });
 
 // console.table method and mySQL is installed and available
--- seed.sql
+++ seed.sql
@@ -1,9 +1,3 @@
--- !!!!!CHANGE TO EMPLOYEES FOR THIS FILE !!!!! -- 
-
-
-
-
-
 DROP DATABASE IF EXISTS employees_db;
 
 CREATE DATABASE employees_db;
@@ -14,55 +8,57 @@ USE employees_db;
 
 CREATE TABLE department (
 
-    id INTEGER AUTO_INCREMENT PRIMARY KEY
-    depName VARCHAR(30) NOT NULL -- department name
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    dep_name VARCHAR(30) NOT NULL -- department name
     
   
 );
 
-CREATE TABLE role (
+CREATE TABLE empRole (
 
-    id INTEGER PRIMARY KEY,
-    name VARCHAR(30) NOT NULL 
-    salary DECIMAL NOT NULL
-    department_id INTEGER(11) -- INT to hold reference to department role belongs to
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL, 
+    salary DECIMAL(10,4) NOT NULL, 
+    dep_id INTEGER NOT NULL -- INT to hold reference to department role belongs to
+    
+    
+    
 
   
 );
 
-CREATE TABLE employee (
+CREATE TABLE employees (
 
-    id INTEGER PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL 
-    last_name VARCHAR(30) NOT NULL
-    role_id INTEGER 
-    manager_id INTEGER(10) --INT to hold reference to another employee that manages the employee being Created. This field may be null if the employee has no manager
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER NULL -- --INT to hold reference to another employee that manages the employee being Created. This field may be null if the employee has no manager
   
 );
 
 -- department id/names
 
-INSERT INTO department (depName)
-VALUES (legal)
-VALUES (enginieering)
-VALUES (human_resources)
-VALUES (design)
+INSERT INTO department (dep_name)
+VALUES("legal"), ("engineering"), ("human_resources"),("design"); -- role ids legal = 10 engineering = 20 human resources = 30 design = 40
 
 
 -- employee role  --
 
-INSERT INTO role (first_name, last_name, role_id, depName)
-VALUES("Nick", "Strong", role_id, department_id)
+INSERT INTO empRole (title, salary, dep_id)
+VALUES("engineering", 100000.00, 20), ("human resources", 150000.00, 30), ("legal", 200000.00, 10), ("design", "175000.00", "40");
 
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
 
-VALUES ("Nick", "Strong", role_id, manager_id)
+VALUES ("Nick", "Strong", 20, 2), ("Khabib", "Nurmagomedov", 30, 3), ("Connor", "McGregor", 10, 1), 
+("Max", "Holloway", "40", "4"), ("Tracy", "Cortez", 20, 2), ("Mackenzie", "Dern", 40, 4), ("Alexa", "Grasso", 10, 1), 
+("Rose","Namajunas","30","3"), ("Rachael","Ostovich","30","3"), ("Valentina","Shevchenko","20","2"), ("Kamaru","Usman","40","4"),
+("Jorge","Masvidal","20","2");
 
 
+USE employees_db;
+SELECT * FROM employees
 
 
-
-INSERT INTO employee (name, first_name, last_name, role_id )
-VALUES ("Ahmed", TRUE, "Rockington", 100);
 
